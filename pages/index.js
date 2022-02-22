ba520e37429e4e47d232d38b8ccb4a62c6060b74
philip <philip.lartey@ecs.co.uk>
2022-02-22 15:54:22 +0000

    before DATABASE is added

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Layout from '../components/Layout';
+import data from '../utils/data';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Typography,
+  CardContent,
+  Button,
+  CardActions,
+} from '@material-ui/core';
+import NextLink from 'next/link';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.title}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions>
+                  <Typography>£{product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
